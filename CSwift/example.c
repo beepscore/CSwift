dbb44c95f0d1991f9d4cb3c8ce945161c55e814e
beepscore <support@beepscore.com>
2019-01-13 11:46:19 -0800

    Add example.c with header file and bridging header.

---
+++ CSwift/example.c
@@ -0,0 +1,9 @@
+//
+//  example.c
+//  CSwift
+//
+//  Created by Steve Baker on 1/13/19.
+//  Copyright © 2019 Beepscore LLC. All rights reserved.
+//
+
+#include "example.h"
---
+++ CSwift/example.h
@@ -0,0 +1,14 @@
+//
+//  example.h
+//  CSwift
+//
+//  Created by Steve Baker on 1/13/19.
+//  Copyright © 2019 Beepscore LLC. All rights reserved.
+//
+
+#ifndef example_h
+#define example_h
+
+#include <stdio.h>
+
+#endif /* example_h */
